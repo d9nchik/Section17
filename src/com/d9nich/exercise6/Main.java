e81ea3e7e51730362f9426b99ff186faf143a8e2
Danylo Halaiko <dan.galaiko@gmail.com>
2020-04-09 13:10:39 +0300

    Exercise6 has been written. I have done a class Loan serializable.

---
+++ src/com/d9nich/exercise6/Main.java
@@ -0,0 +1,20 @@
+package com.d9nich.exercise6;
+
+import java.io.BufferedOutputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+
+public class Main {
+    public static void main(String[] args) {
+        Loan[] loans = new Loan[5];
+        for (int i = 0; i < loans.length; i++)
+            loans[i] = new Loan();
+        try (ObjectOutputStream output = new ObjectOutputStream(new BufferedOutputStream(
+                new FileOutputStream("Exercise17_06.dat")))) {
+            output.writeObject(loans);
+        } catch (IOException ex) {
+            System.out.println("Problem with opening file");
+        }
+    }
+}
